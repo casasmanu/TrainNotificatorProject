2c3b38882dc2e9e4fb4a18ae731a4ba303e4f458
Manu-Debian <manumcasas@gmail.com>
2024-10-03 10:49:14 +0200

    retrieveDepartures working close as fine

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ functions ideas:
 - Abfahrts: monitor that will read the departures in the time that im going to take the train, a few minutes before the desired time, will call the API and send via telegram to the user the status of the station, particullary the train that i should take. Informing about posible delay and info about the platform.
 - night checker : every night will check the plan of the next train that i should take in the morning, to go and come back to the office. informing about possible "dissappearing" of the train or change of plans
 - stations finder: send station name, and when possible type ( stop or station) and retrieves id of the desired station
-
+- Create the schedules in main, to run in the moments that i want ( for now, when im waking up, and getting ready to leave the office, check the trains)
+- prepare to deploy and run in docker ( requisites.txt )
--- traindriver.py
+++ traindriver.py
@@ -6,35 +6,59 @@ import datetime
 
 #DONA - 8000078
 #Pasing - 8004158
+#Munchen - 8000261
 
-headers = {'Accept': 'application/json'}
-params = {'when': 'today 5pm', 'nationalExpress': 'True','national': 'false','bus':'false','regional':'false', 'pretty': 'true','results':'10'}
-station_id="8000078"
-r = requests.get('https://v6.db.transport.rest/stops/'+station_id+'/departures?',params=params, headers=headers)
+def retrieveDepartures(station_id=8000261,desiredDate='5pm'):
+    
+    headers = {'Accept': 'application/json'}
 
-myjson=json.loads(r.text)
+    params = {'when': desiredDate,
+                'duration':30,
+                'nationalExpress': 'True',
+                'national': 'True',
+                'bus':'False',
+                'regional':'True',
+                'regionalExpress':'False',
+                'pretty': 'True',
+                'suburban':'False',
+                'taxi':'False',
+                'results':10}
 
-# Destination---- myjson['departures'][0]['destination']['name']
-# Train         - myjson['departures'][0]['line']['name']
-# When          - myjson['departures'][0]['when']
-# Planned       - myjson['departures'][0]['plannedWhen']
-# myjson['departures']
+    r = requests.get('https://v6.db.transport.rest/stops/'+str(station_id)+'/departures?',params=params, headers=headers,timeout=5)
 
-bahnhof=[]
+    myjson=json.loads(r.text)
 
-for departure in myjson['departures']:
-    TrainName= departure['line']['name']
-    Destination= departure['destination']['name']
-    dateActualDate=datetime.datetime.strptime(departure['when'][:-6],"%Y-%m-%dT%H:%M:%S")
-    ActualDate=datetime.datetime.strftime(dateActualDate,"%H:%M:%S")
-    datePlannedDate=datetime.datetime.strptime(departure['plannedWhen'][:-6],"%Y-%m-%dT%H:%M:%S")
-    PlannedDate=datetime.datetime.strftime(datePlannedDate,"%H:%M:%S")
-    departureImportantData=[TrainName,Destination,ActualDate,PlannedDate,]
-    bahnhof.append(departureImportantData)
-    print (departureImportantData)
+    # Destination---- myjson['departures'][0]['destination']['name']
+    # Train         - myjson['departures'][0]['line']['name']
+    # When          - myjson['departures'][0]['when']
+    # Planned       - myjson['departures'][0]['plannedWhen']
+    # myjson['departures']
+
+    bahnhof=[]
+
+    for departure in myjson['departures']:
+        TrainName= departure['line']['name']
+        Destination= departure['destination']['name']
+        datePlannedDate=datetime.datetime.strptime(departure['plannedWhen'][:-6],"%Y-%m-%dT%H:%M:%S")
+        PlannedDate=datetime.datetime.strftime(datePlannedDate,"%H:%M:%S")
+            
+        if dict.__contains__(departure,'cancelled'):
+
+            departureImportantData=[TrainName,Destination,PlannedDate,"CANCELADO"]
+            bahnhof.append(departureImportantData)
+
+        else:
+            dateActualDate=datetime.datetime.strptime(departure['when'][:-6],"%Y-%m-%dT%H:%M:%S")
+            # myjson['departures'][0]['cancelled']
+            delayTime=departure['when'][-6:]
+            ActualDate=datetime.datetime.strftime(dateActualDate,"%H:%M:%S")
+            Platform=departure['platform']
+            departureImportantData=[TrainName,Destination,PlannedDate,ActualDate+delayTime,Platform]
+            bahnhof.append(departureImportantData)
+    
+        print (departureImportantData)
 
 
 
 
-
-print("done")
+retrieveDepartures()
