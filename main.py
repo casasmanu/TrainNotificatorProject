b994659b19f158ea5729d0d972742e276a5a6dc0
Manu-Debian <manumcasas@gmail.com>
2024-10-05 20:05:31 +0200

    telegram and main updated

--- main.py
+++ main.py
@@ -1,24 +1,51 @@
 import schedule
 import time
+import os
+from dotenv import load_dotenv
+import logging
 from traindriver import retrieveDepartures
 
-# important Stations
-#DONA    ---> 8000078
-#Pasing  ---> 8004158
-#Munchen ---> 8000261
+# ----------------------------Global variables ------------------------------_#
+global botToken,destinatary
 
-# Define a function to print a message
+# Load the .env file
+load_dotenv()
+logger = logging.getLogger(__name__)
+
+#from Drivers.drvConfig import readConfigFile
+
+import Drivers.drvLogger as drvLogger
 
 
-def print_message():
-    print("Hello! It's time to code on GFG")
+drvLogger.initLogger()
+### read config File
+try:
+    botToken = os.environ['BOT_TOKEN']
+    destinatary=os.environ['BOT_DESTINATARY']
+    logger.info("Main.py - environment variables loaded")  
+except:
+    logger.error("Main.py - Error while loading init variables from .env")
 
+retrieveDepartures(station_id=8000261,desiredDate="7am")
 
-# Schedule the task to run every day at 7:00 AM
-schedule.every().day.at("06:20").do(retrieveDepartures(8004158,"7am"))
-schedule.every().day.at("16:20").do(retrieveDepartures(8000078,"5pm"))
-
-while True:
-    schedule.run_pending()
-    time.sleep(10)
+def main():
+    #DONA - 8000078
+    #Pasing - 8004158
+    #Munchen - 8000261
     
+    # Programa las llamadas a las funciones en intervalos diferentes
+    schedule.every().day.at("06:30").do(
+        retrieveDepartures,station_id=8004158,desiredDate="7am")
+    
+    schedule.every().day.at("16:30").do(
+        retrieveDepartures,station_id=8000078,desiredDate="5pm")
+    
+    schedule.every().day.at("22:00").do(
+        retrieveDepartures,station_id=8000261,desiredDate="7am")
+    
+    while True:
+        schedule.run_pending()
+        time.sleep(300)
+
+if __name__ == "__main__":
+    main()
--- traindriver.py
+++ traindriver.py
@@ -1,6 +1,10 @@
 import requests
 import json
 import datetime
+import logging
+from Drivers.drvTelegram import bot_send_msg
+# ----------------------- Variables ----------------------- #
+logger = logging.getLogger(__name__)
 
 #Station Monitor
 
@@ -8,57 +12,61 @@ import datetime
 #Pasing - 8004158
 #Munchen - 8000261
 
-def retrieveDepartures(station_id=8000078,desiredDate='yesterday 5pm'):
+def retrieveDepartures(station_id=8000078,desiredDate='yesterday 5pm',service="nationalExpress",value=True):
     
-    headers = {'Accept': 'application/json'}
-
-    params = {'when': desiredDate,
-                'duration':30,
-                'nationalExpress': 'True',
-                'national': 'True',
-                'bus':'False',
-                'regional':'True',
-                'regionalExpress':'False',
-                'pretty': 'True',
-                'suburban':'False',
-                'taxi':'False',
-                'results':10}
-
-    r = requests.get('https://v6.db.transport.rest/stops/'+str(station_id)+'/departures?',params=params, headers=headers,timeout=5)
-
-    myjson=json.loads(r.text)
-
-    # Destination---- myjson['departures'][0]['destination']['name']
-    # Train         - myjson['departures'][0]['line']['name']
-    # When          - myjson['departures'][0]['when']
-    # Planned       - myjson['departures'][0]['plannedWhen']
-    # myjson['departures']
-
     bahnhof=[]
+    try:    
+        headers = {'Accept': 'application/json'}
 
-    for departure in myjson['departures']:
-        TrainName= departure['line']['name']
-        Destination= departure['destination']['name']
-        datePlannedDate=datetime.datetime.strptime(departure['plannedWhen'][:-6],"%Y-%m-%dT%H:%M:%S")
-        PlannedDate=datetime.datetime.strftime(datePlannedDate,"%H:%M:%S")
-            
-        if dict.__contains__(departure,'cancelled'):
+        params = {'when': desiredDate,
+                'duration':30,
+                'nationalExpress': False,
+                'national': True,
+                'regionalExpress':False,
+                'regional':True,
+                'suburban':False,
+                'bus':False,
+                'subway':False,
+                'tram':False,
+                'taxi':False,
+                'pretty': True,
+                'results':10}
+        
+        params[service]=value
+        logger.info("calling API")
+        r = requests.get('https://v6.db.transport.rest/stops/'+str(station_id)+'/departures?',params=params, headers=headers,timeout=5)
 
-            departureImportantData=[TrainName,Destination,PlannedDate,"CANCELADO"]
-            bahnhof.append(departureImportantData)
+        myjson=json.loads(r.text)
 
-        else:
-            dateActualDate=datetime.datetime.strptime(departure['when'][:-6],"%Y-%m-%dT%H:%M:%S")
-            # myjson['departures'][0]['cancelled']
-            delayTime=departure['when'][-6:]
-            ActualDate=datetime.datetime.strftime(dateActualDate,"%H:%M:%S")
-            Platform=departure['platform']
-            departureImportantData=[TrainName,Destination,PlannedDate,ActualDate+delayTime,Platform]
-            bahnhof.append(departureImportantData)
-    
-        print (departureImportantData)
+        logger("dividing departures and taking only important data")
+        for departure in myjson['departures']:
+            TrainName= departure['line']['name']
+            Destination= departure['destination']['name']
+            datePlannedDate=datetime.datetime.strptime(departure['plannedWhen'][:-6],"%Y-%m-%dT%H:%M:%S")
+            PlannedDate=datetime.datetime.strftime(datePlannedDate,"%H:%M:%S")
+                
+            if dict.__contains__(departure,'cancelled'):
+
+                departureImportantData=[TrainName,Destination,PlannedDate,"CANCELADO"]
+                bahnhof.append(departureImportantData)
+
+            else:
+                dateActualDate=datetime.datetime.strptime(departure['when'][:-6],"%Y-%m-%dT%H:%M:%S")
+                delayTime=departure['when'][-6:]
+                ActualDate=datetime.datetime.strftime(dateActualDate,"%H:%M:%S")
+                Platform=departure['platform']
+                departureImportantData=[TrainName,Destination,PlannedDate,ActualDate+delayTime,Platform]
+                bahnhof.append(departureImportantData)
+        
+            print (departureImportantData)
+    except:
+        bahnhof.append("Error while trying to retrieve station data")
+        logger.error("error while trying to retrieve station data")
+        
+    logger.info("Send message via TELEGRAM")
+    bot_send_msg(bahnhof)
 
 
 
 
-retrieveDepartures()
+#retrieveDepartures()
